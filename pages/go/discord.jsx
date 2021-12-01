8ecb78fd3145ccba5e1c69dcca5baa81b99d15dd
Wayde B <git@alphaxdev.xyz>
2021-12-01 16:09:58 +1100

    Use Tixte invite link

--- pages/go/discord.jsx
+++ pages/go/discord.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class Error404 extends React.Component {
     componentDidMount() {
-        window.location.href = 'https://discord.com/invite/cFZM3EJ3hS';
+        window.location.href = 'https://discord.com/invite/b8pBG86jgY';
     };
     render() {
         return <></>
