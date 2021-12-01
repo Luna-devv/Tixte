c764339685fd49878363786eb23161e20df45eb8
Wayde B <git@alphaxdev.xyz>
2021-12-01 16:09:00 +1100

    Update register.jsx

--- pages/read/register.jsx
+++ pages/read/register.jsx
@@ -16,10 +16,10 @@ const reads = [{
         text: 'Enter a username, your email address, a secure password and a <a href="#code" class="df-link">beta code</a>.'
     }, {
         id: 3,
-        text: 'If you want to use <a href="https://getsharex.com/" class="df-link">shareX</a> or <a href="https://magiccap.me/" class="df-link">magicCap</a> to create screenshots go to the <a href="https://tixte.com/dashboard/configurations" class="df-link">configurations tab</a> to download the respective configs.'
+        text: 'If you want to use <a href="https://getsharex.com/" class="df-link">ShareX</a> or <a href="https://magiccap.me/" class="df-link">MagicCap</a> to create screenshots go to the <a href="https://tixte.com/dashboard/configurations" class="df-link">configurations tab</a> to download the respective configs.'
     }, {
         id: 4,
-        text: 'If you want to link your Tixte account with your discord account, go to <a href="https://discord.com/channels/818940877262553141/818942351946022952" class="mention">#bot-commands</a> on <a href="/go/discord" class="df-link">our server</a> and run <a href="https://cdn.xyna.space/1638297786.png" style="background-color: #2b2b2b; padding: 0.1rem; padding-left: 0.2rem; padding-right: 0.2rem; border-radius: 0.3rem; cursor: pointer">/link [upload_key]</a>.'
+        text: 'If you want to link your Tixte account with your Discord account, go to <a href="https://discord.com/channels/818940877262553141/818942351946022952" class="mention">#bot-commands</a> on <a href="/go/discord" class="df-link">our server</a> and run <a href="https://cdn.xyna.space/1638297786.png" style="background-color: #2b2b2b; padding: 0.1rem; padding-left: 0.2rem; padding-right: 0.2rem; border-radius: 0.3rem; cursor: pointer">/link [upload_key]</a>.'
     }],
     buttons: [{
         url: '/',
@@ -37,10 +37,10 @@ const reads = [{
         text: 'Enter a username, your email address, a secure password and a <a href="#code" class="df-link">beta code</a>.'
     }, {
         id: '•',
-        text: 'If you want to use <a href="https://getsharex.com/" class="df-link">shareX</a> or <a href="https://magiccap.me/" class="df-link">magicCap</a> to create screenshots go to the <a href="https://tixte.com/dashboard/configurations" class="df-link">configurations tab</a> to download the respective configs.'
+        text: 'If you want to use <a href="https://getsharex.com/" class="df-link">ShareX</a> or <a href="https://magiccap.me/" class="df-link">MagicCap</a> to create screenshots go to the <a href="https://tixte.com/dashboard/configurations" class="df-link">configurations tab</a> to download the respective configs.'
     }, {
         id: '•',
-        text: 'If you want to link your Tixte account with your discord account, go to <a href="https://discord.com/channels/818940877262553141/818942351946022952" class="mention">#bot-commands</a> on <a href="/go/discord" class="df-link">our server</a> and run <a href="https://cdn.xyna.space/1638297786.png" style="background-color: #2b2b2b; padding: 0.1rem; padding-left: 0.2rem; padding-right: 0.2rem; border-radius: 0.3rem; cursor: pointer">/link [upload_key]</a>.'
+        text: 'If you want to link your Tixte account with your Discord account, go to <a href="https://discord.com/channels/818940877262553141/818942351946022952" class="mention">#bot-commands</a> on <a href="/go/discord" class="df-link">our server</a> and run <a href="https://cdn.xyna.space/1638297786.png" style="background-color: #2b2b2b; padding: 0.1rem; padding-left: 0.2rem; padding-right: 0.2rem; border-radius: 0.3rem; cursor: pointer">/link [upload_key]</a>.'
     }],
     buttons: [{
         url: '/',
