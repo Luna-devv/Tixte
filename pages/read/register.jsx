7453a30477ad4baefef71f187e85ab3fac9210f1
Wayde B <git@alphaxdev.xyz>
2021-12-03 15:49:57 +1100

    Update to reflect latest changes

--- pages/read/register.jsx
+++ pages/read/register.jsx
@@ -5,7 +5,7 @@ import {
     ExternalLinkIcon
 } from '@heroicons/react/outline';
 
-const reads = [{
+const json = [{
     title: 'Register',
     section: 'how',
     list: [{
@@ -55,37 +55,7 @@ export default class Homepage extends React.Component {
 
     render() {
         return (
-            <>
-                <div className='read'>
-                {reads?.map(read => (
-                    <section id={read?.section} className='readContainer'>
-                        <a className='readTitle' href={'/read/register#' + read?.section} style={{cursor: 'pointer'}}> {read?.title} <DuplicateIcon className='copyIco ico' /> </a> <br />
-                        <div className='readContent'>
-                        {read?.list?.map(item => (
-                            <>
-                                 <id style={{color: '#ababab'}}>{item?.id}{isNaN(item?.id) ? '' : '.'}</id> <text dangerouslySetInnerHTML={{__html: item?.text}}></text> <br />
-                                <div style={item?.subs ? {marginBottom: 10} : {}}>
-                                    {item?.subs?.map(sub => (
-                                        <>
-                                                <text dangerouslySetInnerHTML={{__html: sub}}></text> <br />
-                                        </>
-                                    ))}
-                                </div>
-                            </>
-                        ))}
-                        </div>
-                        <br />
-                            {read?.buttons?.map(button => (
-                                <>
-                                    <button className='button' style={button?.url && button?.text ? {marginLeft: 14} : {visibility: 'hidden', position: 'absolute'}} onClick={()=> window.open(button?.url)}>
-                                        {button?.text} <ExternalLinkIcon className='ico' style={{top: 3.2, left: 0.1}} />
-                                    </button>
-                                </>
-                            ))}
-                    </section>
-                ))}
-                </div>
-            </>
+            <Reads data={json} />
         );
     };
 };
