b3f90ddde2645eb6f8442748ea20396066177ee0
Max <59715207+mynameismax@users.noreply.github.com>
2022-11-16 16:55:29 -0500

    fixed some typos, updated some information

--- pages/read/register.jsx
+++ pages/read/register.jsx
@@ -12,10 +12,11 @@ const json = {
             text: 'Go to <a href="https://tixte.com/register" class="link">tixte.com/register</a>.'
         }, {
             id: 2,
-            text: 'Enter a cool username, your email address, and a secure password.'
+            text: 'Enter your desired username, your email address, and a secure password.'
         }, {
             id: 3,
-            text: 'If you want to use <a href="https://getsharex.com/" class="df-link">ShareX</a> or <a href="https://magiccap.me/" class="df-link">MagicCap</a> (Use Tixte Snap on macOS) to create screenshots go to the <a href="https://tixte.com/dashboard/configurations" class="df-link">configurations tab</a> to download the respective configs.'
+            text: 'If you want to use <a href="https://getsharex.com/" class="df-link">ShareX</a> or <a href="https://magiccap.me/" class="df-link">MagicCap (no longer supported or recommended)</a> to take and upload screenshots, go to the <a href="https://tixte.com/dashboard/integrations" class="df-link">Integrations tab</a> to download the respective configs.',
+            subs: ['If you use macOS, we recommend using Tixte Snap to take and upload screenshots. <a href="https://tixte.com/dashboard/integrations" class="df-link">You can download it here</a>.']
         }, {
             id: 4,
             text: 'If you want to link your Tixte account with your Discord account, go to <a href="https://discord.com/channels/818940877262553141/818942351946022952" class="mention">#bot-commands</a> on our <a href="/go/discord" class="df-link">discord server</a> and run <a class="highlight">/link [upload_key]</a>.',
